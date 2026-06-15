226063bf188520965e657ce7b4c7d997680d9c3a
GCV <GCV@example.com>
2026-06-16 00:30:00 +0530

    Fix Syntax Error in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
-
-export default { eslint: { ignoreDuringBuilds: true }, typescript: { ignoreBuildErrors: true } };
